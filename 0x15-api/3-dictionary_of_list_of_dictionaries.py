34a7ca9f339ffce31507d56c8201dc354e7322e6
julioceaseless <julioceaseless@gmail.com>
2024-03-26 04:27:50 +0000

    retrieve all records

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -7,10 +7,10 @@ import json
 import requests
 import sys
 
+
 def records(tasks_dict, users_dict):
     """
     counts the number of tasks completed by user
-    
     @tasks_dict: dictionary of todo list
     @users_dict: dictionary of user details
     """
@@ -18,34 +18,28 @@ def records(tasks_dict, users_dict):
     list_of_tasks = []
     dict_of_lists = {}
     for user in users_dict:
+        user_id = user["id"]
         for _dict in tasks_dict:
-            print(_dict["userId"])
-            print(user[1])
-            if _dict["userId"] == user[1]:
+            if _dict["userId"] == user_id:
                 # retrieve all entries for specific user ID
                 user_task_dict = {
+                        "username": user['username'],
                         "task": _dict['title'],
-                        "completed": _dict['completed'],
-                        "username": user['username']
+                        "completed": _dict['completed']
                         }
                 # store entries in a list
                 list_of_tasks.append(user_task_dict)
-        dict_of_lists[user["userId"]] = list_of_tasks
+        dict_of_lists[user_id] = list_of_tasks
+    print(dict_of_lists)
     return dict_of_lists
 
 
 if __name__ == "__main__":
     """ run script only when executed directly"""
-    try:
-        user_id = int(sys.argv[1])
-    except ValueError:
-        print(f"Usage: {sys.argv[0]} NUM")
-        exit(1)
+    USER_URL = f"https://jsonplaceholder.typicode.com/users/"
+    TODO_URL = f"https://jsonplaceholder.typicode.com/todos/"
+    file_path = "todo_all_employees.json"
 
-    USER_URL = f"https://jsonplaceholder.typicode.com/users/{user_id}"
-    TODO_URL = f"https://jsonplaceholder.typicode.com/todos/?userID={user_id}"
-    file_path = f"{user_id}.json"
-    
     # get user's username
     response_user = requests.get(USER_URL)
     users = response_user.json()
