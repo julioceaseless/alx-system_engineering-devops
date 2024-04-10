5487657ba20bb005ddae78b338fee3176bda0b8f
julioceaseless <julioceaseless@gmail.com>
2024-04-10 13:45:43 +0000

    minor change

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,12 +10,13 @@ def number_of_subscribers(subreddit):
     '''Retrieve the number of subscribers'''
     headers = {'User-Agent': 'app/v1.0.0'}
     url = "https://reddit.com/r/{}/about.json".format(subreddit)
+
+    # perform GET request to API without following redirect
     res = requests.get(url, headers=headers, allow_redirects=False)
-    if (res.status_code == 200 and not res.is_redirect):
-        # print subscriber count
-        try:
-            return int(res.json()['data']['subscribers'])
-        except KeyError:
-            return 0
+    if (res.status_code == 200):
+        # print the number of subscribers
+        data = res.json().get('data', {})
+        subscribers = data.get('subscribers', 0)
+        return subscribers
     else:
         return 0
